07d0ca9db147d955c527fdba84609af8df158b07
cth2888 <chentinghong22@mails.ucas.ac.cn>
2025-05-11 23:10:25 +0800

    Adds KL loss to basic SFT

---
+++ examples/train_lora/run_sft.sh
@@ -0,0 +1,5 @@
+export ALLOW_EXTRA_ARGS=1
+export NCCL_DEBUG=INFO
+export NCCL_NVLS_ENABLE=0
+cd /dev/shm/cth/LLaMA-Factory
+llamafactory-cli train examples/train_lora/llama3_lora_sft_ds3.yaml > terminal.txt #2>&1
