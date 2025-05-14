d47ef7103fd0c545e99ac87c15f3fd397923e909
Guo Xinyuan <guoxinyuan23@mails.ucas.ac.cn>
2025-05-14 21:31:12 +0800

    Fix bugs in visualizations

--- examples/train_lora/run_sft.sh
+++ examples/train_lora/run_sft.sh
@@ -1,5 +1,5 @@
 export ALLOW_EXTRA_ARGS=1
 export NCCL_DEBUG=INFO
 export NCCL_NVLS_ENABLE=0
-cd ~/cth/cth/LLaMA-Factory
-llamafactory-cli train examples/train_lora/llama3_lora_sft_ds3.yaml > terminal.txt #2>&1
+cd ~/cth/LLaMA-Factory
+llamafactory-cli train examples/train_lora/llama3_lora_sft_ds3.yaml
