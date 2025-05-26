12d27e93fd43cab97f01bf896511173c4c875086
ericguoxy <guoxinyuan23@mails.ucas.ac.cn>
2025-05-26 15:51:58 +0800

    Add curriculum sft+eval files

---
+++ examples/train_full/run_7b_full_curr.sh
@@ -0,0 +1,29 @@
+export ALLOW_EXTRA_ARGS=1
+export NCCL_DEBUG=INFO
+export NCCL_NVLS_ENABLE=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+# Train the model
+cd ~/cth/LLaMA-Factory
+llamafactory-cli train examples/train_full/qwen2_5_math_7b_full_curr_part1.yaml
+
+# Evaluate the model
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+bash evaluation/eval_7b_curr_part1.sh
+
+# Train the model
+cd ~/cth/LLaMA-Factory
+llamafactory-cli train examples/train_full/qwen2_5_math_7b_full_curr_part2.yaml
+
+# Evaluate the model
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+bash evaluation/eval_7b_curr_part2.sh
+
+# Train the model
+cd ~/cth/LLaMA-Factory
+llamafactory-cli train examples/train_full/qwen2_5_math_7b_full_curr_part3.yaml
+
+# Evaluate the model
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+bash evaluation/eval_7b_curr_part3.sh
+
